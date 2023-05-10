de871d0018275c796a9584f3b693a3c2eb1ea90c
Ali mousavi <mousavi.ali79.am@gmail.com>
2023-05-10 09:05:47 +0200

    sql rel add show info in admin

---
+++ product_module/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from . import models
+
+
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    # readonly_fields = ['slug']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+
+    list_display = ['__str__', 'price', 'rating', 'is_active', 'category']
+    list_filter = ['rating', 'is_active']
+    list_editable = ['rating', 'is_active']
+
+
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.ProductCategory)
+admin.site.register(models.ProductInformation)
---
+++ product_module/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
+
+
+# Create your models here.
+
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=300, verbose_name='عنوان')
+    url_title = models.CharField(max_length=300, verbose_name='عنوان در url')
+
+    def __str__(self):
+        return f'( {self.title} - {self.url_title} )'
+
+
+class ProductInformation(models.Model):
+    color = models.CharField(max_length=200, verbose_name='رنگ')
+    size = models.CharField(max_length=200, verbose_name='سایز')
+
+    def __str__(self):
+        return f'{self.size} - {self.color}'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=300)
+    product_information = models.OneToOneField(
+        ProductInformation,
+        on_delete=models.CASCADE,
+        related_name='product_information',
+        verbose_name='اطلاعات تکمیلی',
+        null=True,
+        blank=True)
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='products',
+        verbose_name='دسته بندی')
+    price = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=0)
+    short_description = models.CharField(max_length=360, null=True)
+    is_active = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('product-detail', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.title} ({self.price})"
