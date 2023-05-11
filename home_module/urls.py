3a1de178239652b8bf3648be1e9e0afaf1bb3ad2
Ali mousavi <mousavi.ali79.am@gmail.com>
2023-05-11 15:36:47 +0200

    Review of django render partial

--- home_module/urls.py
+++ home_module/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index_page),
     path('contact-us', views.contact_page),
+    # path('site-hearder', views.site_hearder_partial, name='site_header_partial'),
 ]
