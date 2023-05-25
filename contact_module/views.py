2a3b942c131bcec5d25eda72320990c45ac9aa1c
Alimousavi47 <mousavi.ali79.am@gmail.com>
2023-05-25 13:48:59 +0200

    Checking file upload in Django

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import ContactUsModelForm
+from .forms import ContactUsModelForm, ProfileForm
 from django.views.generic.edit import FormView, CreateView
 
-from .models import ContactUs
+from .models import ContactUs, UserProfile
 
 
 class ContactUsView(CreateView):
@@ -12,10 +12,28 @@ class ContactUsView(CreateView):
     success_url = '/contact-us/'
 
 
+def store_file(file):
+    with open('temp/image.jpg', "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, 'contact_module/create_profile_page.html')
+        form = ProfileForm()
+        return render(request, 'contact_module/create_profile_page.html', {
+            'form': form
+        })
 
     def post(self, request):
-        print(request.FILES)
-        return redirect('/contact-us/create-profile')
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            # store_file(request.FILES['profile'])
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            return redirect('/contact-us/create-profile')
+
+        return render(request, 'contact_module/create_profile_page.html', {
+            'form': submitted_form
+        })
