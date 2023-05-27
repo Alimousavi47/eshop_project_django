ca4c61cfc14cf767be3b81a0f43be2ede772e039
Alimousavi47 <mousavi.ali79.am@gmail.com>
2023-05-27 14:00:50 +0200

    Examining the concept of Session

--- product_module/views.py
+++ product_module/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic.base import TemplateView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic.base import TemplateView, View
 from django.views.generic import ListView, DetailView
 from .models import Product
 
@@ -19,3 +19,10 @@ class ProductDetailView(DetailView):
     template_name = 'product_module/product_detail.html'
     model = Product
 
+
+class AddProductFavorite(View):
+    def post(self, request):
+        product_id = request.POST["product_id"]
+        product = Product.objects.get(pk=product_id)
+        request.session["product_favorite"] = product_id
+        return redirect(product.get_absolute_url())
