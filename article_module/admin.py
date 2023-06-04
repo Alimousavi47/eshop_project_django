ac391dc70701ababb2077bdce8f0d97eb92abceb
Alimousavi47 <mousavi.ali79.am@gmail.com>
2023-06-04 11:40:09 +0200

    Display the list of articles

--- article_module/admin.py
+++ article_module/admin.py
@@ -7,5 +7,11 @@ class ArticleCategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'url_title', 'parent', 'is_active']
     list_editable = ['url_title', 'parent', 'parent']
 
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'is_active']
+    list_editable = ['is_active']
+
+
 admin.site.register(models.ArticleCategory, ArticleCategoryAdmin)
-admin.site.register(models.Article)
+admin.site.register(models.Article, ArticleAdmin)
--- article_module/urls.py
+++ article_module/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path()
+    #path('', views.ArticlesView.as_view(), name='articles_list')
+    path('', views.ArticlesListView.as_view(), name='articles_list')
+    
 ]
