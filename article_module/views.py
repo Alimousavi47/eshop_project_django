9b710ad2b1abd09d8d8eb5fe7b20892a7d7885ee
Alimousavi47 <mousavi.ali79.am@gmail.com>
2023-06-05 09:17:14 +0200

    Check the solar date

--- article_module/views.py
+++ article_module/views.py
@@ -1,7 +1,10 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views import View
 from article_module.models import Article
 from django.views.generic.list import ListView
+from jalali_date import datetime2jalali, date2jalali
+
 # Create your views here.
 
 # class ArticlesView(View):
@@ -16,3 +19,8 @@ class ArticlesListView(ListView):
     model = Article
     paginate_by = 5
     template_name  = 'article_module/articles_page.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ArticlesListView, self).get_context_data(*args, **kwargs)
+        context['date'] = datetime2jalali(self.request.user.date_joined)
+        return context
--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'article_module',
     # external apps
     'django_render_partial',
+    'jalali_date',
     'sorl.thumbnail'
 ]
 
@@ -145,3 +146,29 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'onionarchitecturemvc@gmail.com'
 EMAIL_HOST_PASSWORD = 'djangotoplearn@pass'
 EMAIL_PORT = 587
+
+# default settings (optional)
+JALALI_DATE_DEFAULTS = {
+   'Strftime': {
+        'date': '%y/%m/%d',
+        'datetime': '%H:%M:%S _ %y/%m/%d',
+    },
+    'Static': {
+        'js': [
+            # loading datepicker
+            'admin/js/django_jalali.min.js',
+            # OR
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
+            # 'admin/js/main.js',
+        ],
+        'css': {
+            'all': [
+                'admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.min.css',
+            ]
+        }
+    },
+}
+
