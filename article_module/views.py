f94bfa85d696195c15280556bf88e4f7022be860
Alimousavi47 <mousavi.ali79.am@gmail.com>
2023-06-05 18:43:03 +0330

    Show article comments on the page

--- article_module/views.py
+++ article_module/views.py
@@ -6,7 +6,7 @@ from django.views.generic import DetailView
 from django.views import View
 from django.views.generic.list import ListView
 from jalali_date import datetime2jalali, date2jalali
-from article_module.models import Article, ArticleCategory
+from article_module.models import Article, ArticleCategory, ArticleComment
 
 # Create your views here.
 
@@ -43,6 +43,14 @@ class ArticleDetailView(DetailView):
         query = super(ArticleDetailView, self).get_queryset()
         query = query.filter(is_active=True)
         return query
+    
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data()
+        article: Article = kwargs.get('object')
+        #context['comments'] = ArticleComment.objects
+        #print(kwargs)
+        context['comments'] = ArticleComment.objects.filter(article_id=article.id, parent=None).prefetch_related('articlecomment_set')
+        return context
 
 
 def article_categories_component(request: HttpRequest):
