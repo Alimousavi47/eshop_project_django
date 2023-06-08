cb396bf9576d6e9c949f396facca8e0c503513ff
Alimousavi47 <mousavi.ali79.am@gmail.com>
2023-06-08 12:30:40 +0330

    Changing the user password in the user panel

--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
 from account_module.models import User
-from .forms import EditProfileModelForm
-
+from .forms import EditProfileModelForm, ChangePasswordForm
+from django.contrib.auth import logout
 
 class UserPanelDashboardPage(TemplateView):
     template_name = 'user_panel_module/user_panel_dashboard_page.html'
@@ -31,6 +32,31 @@ class EditUserProfilePage(View):
             'current_user': current_user
         }
         return render(request, 'user_panel_module/edit_profile_page.html', context)
+
+
+class ChangePasswordPage(View):
+    def get(self, request: HttpRequest):
+        context = {
+            'form': ChangePasswordForm()
+        }
+        return render(request, 'user_panel_module/change_password_page.html', context)
+
+    def post(self, request: HttpRequest):
+        form = ChangePasswordForm(request.POST)
+        if form.is_valid():
+            current_user: User = User.objects.filter(id=request.user.id).first()
+            if current_user.check_password(form.cleaned_data.get('current_password')):
+                current_user.set_password(form.cleaned_data.get('password'))
+                current_user.save()
+                logout(request)
+                return redirect(reverse('login_page'))
+            else:
+                form.add_error('password', 'کلمه عبور وارد شده اشتباه می باشد')
+
+        context = {
+            'form': form
+        }
+        return render(request, 'user_panel_module/change_password_page.html', context)
 
 
 def user_panel_menu_component(request: HttpRequest):
