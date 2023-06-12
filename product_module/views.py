1796d424e18eaaf75fdb7adb31d9c2b02e8d3f73
Alimousavi47 <mousavi.ali79.am@gmail.com>
2023-06-12 09:13:14 +0330

    Product price filter

--- product_module/views.py
+++ product_module/views.py
@@ -11,7 +11,7 @@ class ProductListView(ListView):
     model = Product
     context_object_name = 'products'
     ordering = ['-price']
-    paginate_by = 6
+    paginate_by = 1
 
     def get_context_data(self, *, object_list=None, **kwargs):
         print('context_data')
